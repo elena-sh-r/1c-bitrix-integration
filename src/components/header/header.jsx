97d08681635de4a8f667b627f1459d3ff0715f56
Elena Sharipova <elena.sh.r@yandex.ru>
2022-04-13 15:25:33 +0300

    Header separated

---
+++ src/components/header/header.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import headerStyles from './header.module.css';
+
+import logo from '../../images/logo.svg';
+
+const Header = () => {
+  return (
+    <header className={`${headerStyles['header']}`}>
+      <div className={`${headerStyles['container']}`}>
+        <div className={`${headerStyles['header__row']}`}>
+          <div className={`${headerStyles['header__group']}`}>
+            <a href="/" className={`${headerStyles['header__logo']}`}>
+              <img src={logo} alt="" width="79px" />
+            </a>
+            <div className={`${headerStyles['hamburger']} ${headerStyles['hamburger--squeeze']} ${headerStyles['header__hamburger']}`}>
+              <div className={`${headerStyles['hamburger-box']}`}>
+                <div className={`${headerStyles['hamburger-inner']}`}></div>
+              </div>
+            </div>
+          </div>
+          <div className={`${headerStyles['header-menu']}`}>
+            <div className={`${headerStyles['header-menu__container']}`}>
+              <ul className={`${headerStyles['header-menu__list']}`}>
+                <li><a href="#anchor-1">С кем работаем</a></li>
+                <li><a href="#anchor-2">Для кого</a></li>
+                <li><a href="#anchor-3">Услуги</a></li>
+                <li><a href="#anchor-4">Схема работы</a></li>
+                <li><a href="#anchor-5">Цены</a></li>
+                <li><a href="#anchor-6">Команда</a></li>
+                <li><a href="#anchor-7">Отзывы</a></li>
+                <li><a href="#anchor-8">FAQ</a></li>
+              </ul>
+            </div>
+          </div>
+          <a href="tel:8800787447" className={`${headerStyles['header__phone']}`}>8 800 787 447</a>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
